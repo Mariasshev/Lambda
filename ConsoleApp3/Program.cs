f1c443b552d48aabca8b0e88611a2d270235c2a0
Maria Shevchenko <mariassheva@gmail.com>
2024-11-04 20:50:52 +0200

    res

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
-    delegate int[] Multiples(int[] arr);
     class MyArr
     {
         private int[] arr;
@@ -42,7 +42,35 @@ namespace ConsoleApp3
             }
             Console.WriteLine(" ");
         }
-
+        
+        public static string[] SplitIntoWords(string text)
+        {
+            int count = 0;
+            for(int i = 0; i <  text.Length; i++)
+            {
+                if(text[i] == ' ' || text[i] == '!' || text[i] == ',' || text[i] == '.')
+                {
+                    count++;
+                }
+            }
+            string[] words = new string[count];
+            int count2 = 0;
+            string current = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ' || text[i] == '!' || text[i] == ',' || text[i] == '.')
+                {
+                    words[count2] = current.ToLower();
+                    current = "";
+                    count2++;
+                }
+                else
+                {
+                    current += text[i];
+                }
+            }
+            return words;
+        }
         public static void Main(string[] args)
         {
             MyArr myArr = new MyArr(10);
@@ -50,8 +78,27 @@ namespace ConsoleApp3
 
             Console.WriteLine("Числа кратные 7 (количество): " + myArr.GetArr().Where(num => num % 7 == 0).Count());
             Console.WriteLine("Положительные числа (количество): " + myArr.GetArr().Where(num => num > 0).Count());
-            Console.WriteLine("Уникальные отрицательные числа: " + string.Join(", ", myArr.GetArr().Where(num => num < 0)));
 
+            var uniqueNegativeNumbers = myArr.GetArr()
+                .Where(num => num < 0)               
+                .GroupBy(num => num)                
+                .Where(group => group.Count() == 1)  
+                .Select(group => group.Key);        
+
+            Console.WriteLine("Уникальные отрицательные числа: " + string.Join(", ", uniqueNegativeNumbers));
+
+            string text = "Hello World! So happy to see all of you!";
+            string[] words = SplitIntoWords(text);
+            Console.WriteLine("Слова в строке:");
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            string needWord = "To";
+
+            bool result = words.Any(word => needWord.ToLower() == word);
+            Console.WriteLine("Поиск слова " + needWord + ": " + result);
 
         }
 
